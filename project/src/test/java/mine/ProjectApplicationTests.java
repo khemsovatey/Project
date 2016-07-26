f9dc681ee3a772b6d18e1f5036aa3e24be0e4327
khemsovatey <khem.sovatey@yahoo.com>
2016-07-26 16:27:57 +0700

    Project

---
+++ project/src/test/java/mine/ProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package mine;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ProjectApplication.class)
+public class ProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
